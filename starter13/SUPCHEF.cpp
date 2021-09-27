9eb6728c095522f76b844b6e89abc69b0761bb76
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-09-27 22:28:29 +0530

    sup chief

---
+++ starter13/SUPCHEF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, m, n, k;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> m >> n >> k;
+        if (m > n * k)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
