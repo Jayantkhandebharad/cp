9bb15143f8d9b188c6a0f8c857628a6fbab4458d
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-10-07 21:53:17 +0530

    aasraeasa

---
+++ SDEsheet/day5_5_delGiveNode.cpp
@@ -0,0 +1,26 @@
+/*Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, 
+instead you will be given access to the node to be deleted directly.
+It is guaranteed that the node to be deleted is not a tail node in the list.*/
+
+//problem with: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode *node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
