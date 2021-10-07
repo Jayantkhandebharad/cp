d766dcc59ca5a7a9fcc2abcf17274a9a3b5e49e6
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-10-07 19:16:27 +0530

    aasda

--- SDEsheet/day5_1_reverseLinkedlist.cpp
+++ SDEsheet/day5_1_reverseLinkedlist.cpp
@@ -5,16 +5,14 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution
 {
 public:
