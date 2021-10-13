b0d17dc85b0c2b7042598e5de96266d545c99da7
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-10-13 16:58:07 +0530

    dsdfe

---
+++ FRAZ_sheet/searchRotSorted.cpp
@@ -0,0 +1,56 @@
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+//problem link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int right = nums.size() - 1;
+        int left = 0;
+        int mid;
+        while (left <= right)
+        {
+            mid = (left + right) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[left] <= nums[mid])
+            { //if left part is sorted
+                if (target >= nums[left] and target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            if (nums[right] > nums[mid])
+            {
+                if (target > nums[mid] and target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
