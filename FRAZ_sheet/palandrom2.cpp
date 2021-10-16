723623f5071f16f9bc2c43a32c22811ca2841a57
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-10-16 09:47:53 +0530

    valid-palindrome-ii

---
+++ FRAZ_sheet/palandrom2.cpp
@@ -0,0 +1,48 @@
+/*Given a string s, return true if the s can be palindrome after deleting at most one character from it.*/
+
+//problem link: https://leetcode.com/problems/valid-palindrome-ii/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool validPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left <= right)
+        {
+
+            if (s[left] != s[right])
+            {
+
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    bool isPalindrome(string s, int start, int end)
+    {
+
+        while (start <= end)
+        {
+
+            if (s[start] != s[end])
+            {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
