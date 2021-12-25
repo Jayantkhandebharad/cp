5fb44b853d6add9910f7f92b785c200f289ed8e3
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-12-25 23:26:34 +0530

    december lunchtime added

---
+++ suptember_lunchtime_cc/OPTSORT.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t, n;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        vector<int> x(n), y(n);
+        for (int j = 0; j < n; j++)
+        {
+            cin >> x[i];
+        }
+        }
+    return 0;
+}
