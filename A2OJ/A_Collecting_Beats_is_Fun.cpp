32cada37cd1f5a83a9911e880a95c2c1c6c4210d
jayantkhandebharad <jntkhandebharad@gmail.com>
2021-12-28 10:03:47 +0530

    A2oj 63 64th done added

--- A2OJ/A_Collecting_Beats_is_Fun.cpp
+++ A2OJ/A_Collecting_Beats_is_Fun.cpp
@@ -1,8 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a,b,c,d;
-    vector<string> aa;
+int main()
+{
+    int aa[10] = {0};
+    int k;
+    cin >> k;
+    char x;
+    for (int i = 0; i < 16; i++)
+    {
+        cin >> x;
+        if (x != '.')
+        {
+            aa[x - '0']++;
+        }
+    }
+    bool flag = false;
+    for (int i = 0; i < 10; i++)
+    {
+        if (aa[i] > 2 * k)
+        {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
     return 0;
 }
---
+++ A2OJ/A_Levko_and_Table.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    int i, j;
+
+    cin >> n >> k;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i == j)
+                cout << k << " ";
+            else
+                cout << "0 ";
+        }
+
+        cout << endl;
+    }
+}
