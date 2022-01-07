b5f6c1f2666202768fcc88e03af011cd5598afc4
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-01-07 08:25:07 +0530

    power of 2 450

---
+++ lovebabar450/powerOf2.cpp
@@ -0,0 +1,30 @@
+/*
+Power of 2 
+
+Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some x.
+*/
+
+class Solution
+{
+public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n)
+    {
+
+        // Your code here
+        if (n < 0)
+            return false;
+
+        int cnt = 0;
+        while (n)
+        {
+            cnt++;
+            n = n & (n - 1);
+        }
+        if (cnt == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+};
