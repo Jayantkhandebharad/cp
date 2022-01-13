9c1ef2e8aa23a5ea103731054f1f76958b5fcefa
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-01-13 09:28:07 +0530

    running array sum

---
+++ FRAZ_sheet/running_array_sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+};
