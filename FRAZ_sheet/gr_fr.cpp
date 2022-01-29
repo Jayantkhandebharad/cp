5cb15206881b8091859c1c3aee9b3dc55e949dbc
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-01-29 15:49:24 +0530

    gr fr

---
+++ FRAZ_sheet/gr_fr.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n, int m)
+{
+    vector<vector<int>> v(n, vector<int>(m));
+    for (int i = 0; i < n)
+}
+
+int main()
+{
+    int t, n, m;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> m;
+        solve(n, m);
+    }
+}
