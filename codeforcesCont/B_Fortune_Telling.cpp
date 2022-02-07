7809d2c508d76b736264aaf32f78209d3b37d525
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-02-08 00:03:02 +0530

    b. fortune telling contest 770 codeforces

--- codeforcesCont/B_Fortune_Telling.cpp
+++ codeforcesCont/B_Fortune_Telling.cpp
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool f(long long x, long long y, vector<long long> &a, long long i)
-{
-    if (x == y and a.size() == i)
-    {
-        return true;
-    }
-    else if (i == a.size())
-    {
-        return false;
-    }
-    //cout << x << " ";
-    return f(x + a[i], y, a, i + 1) || f(x ^ a[i], y, a, i + 1);
-}
-
 string solve(long long n, long long x, long long y)
 {
+    int e = 0, o = 0;
     vector<long long> a(n);
     for (long long i = 0; i < n; i++)
     {
         cin >> a[i];
+        if (a[i] % 2 == 0)
+        {
+            e++;
+        }
+        else
+        {
+            o++;
+        }
     }
-    if (f(x, y, a, 0))
+    if ((x % 2 == 0 and o % 2 == 0 and y % 2 == 0) || (x % 2 == 1 and o % 2 == 0 and y % 2 == 1) || (x % 2 == 1 and o % 2 == 1 and y % 2 == 0) || (x % 2 == 0 and o % 2 == 1 and y % 2 == 1))
     {
         return "Alice";
     }
