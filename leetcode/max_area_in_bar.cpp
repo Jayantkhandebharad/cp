6c0f7cf5609c2f243df9105ff4978ff36a5b98dc
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-02-08 01:54:50 +0530

    max area in bars problem added

---
+++ leetcode/max_area_in_bar.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int> &height)
+{
+    int left = 0;
+    int right = height.size() - 1;
+    int area = 0;
+    while (left < right)
+    {
+        area = max(area, min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+    return area;
+}
