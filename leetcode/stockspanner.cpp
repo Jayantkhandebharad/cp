a80a9197c4df4f55d5ff48b96793bb4f7c5fdd2c
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-03-12 09:04:34 +0530

    next greater leet

---
+++ leetcode/stockspanner.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class StockSpanner
+{
+public:
+    stack<pair<int, int>> st;
+    StockSpanner()
+    {
+    }
+
+    int next(int price)
+    {
+        int span = 1;
+        while (!st.empty() and st.top().first <= price)
+        {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
