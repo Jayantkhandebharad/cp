e27170c920917a2eb5036dcc6321cc56ee391744
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-04-02 16:25:44 +0530

    factorial trailing zero

--- lovebabar450/factorial_trailing_zero.cpp
+++ lovebabar450/factorial_trailing_zero.cpp
@@ -11,8 +11,7 @@ public:
         int count = 0;
         for (int i = 5; (num / i) > 0; i = i * 5)
         {
-                        count = count + num / i;
-                    
+            count = count + num / i;
         }
 
         return count;
