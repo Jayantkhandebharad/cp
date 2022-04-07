10e6f82fd2bcbd70a3c9fd14e069f72e32415c3f
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-04-07 17:13:12 +0530

    array of permutation

---
+++ lovebabar450/array_of_permutation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        vector<int> ans(nums.size());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+};
