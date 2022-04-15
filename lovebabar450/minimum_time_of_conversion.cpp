a18e3fbf9776a2724a80ecedd92d992ccc357666
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-04-15 19:12:17 +0530

    minimum opration to convert time

---
+++ lovebabar450/minimum_time_of_conversion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int gettime(string &s)
+    {
+        return stoi(s.substr(0, 2)) * 60 + stoi(s.substr(3.5));
+    }
+
+    int convertTime(string current, string correct)
+    {
+        int diff = gettime(current) - gettime(correct);
+        if (diff < 0)
+        {
+            diff = diff * (-1);
+        }
+        int ans = diff / 60;
+        diff = diff % 60;
+        ans += diff / 15;
+        diff = diff % 15;
+        ans += diff / 5;
+        diff = diff % 5;
+        ans += diff;
+        return ans;
+    }
+};
