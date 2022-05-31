f5f0115af5ab0eadf2f605ef454737e195300dea
jayantkhandebharad <jntkhandebharad@gmail.com>
2022-05-31 11:28:16 +0530

    path sum

--- may/pathsum.cpp
+++ may/pathsum.cpp
@@ -9,17 +9,16 @@ void solve(){
 int main(){
     solve();
 }			
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
